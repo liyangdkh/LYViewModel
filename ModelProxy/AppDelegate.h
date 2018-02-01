8bfed831b6120621628a3051fe57ecd4f0edbf9b
shyzh_li@163.com <shyzh_li@163.com>
2018-02-01 16:10:51 +0800

    Initial Commit

---
+++ ModelProxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModelProxy
+//
+//  Created by liyang on 2018/2/1.
+//  Copyright © 2018年 dashang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModelProxy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModelProxy
+//
+//  Created by liyang on 2018/2/1.
+//  Copyright © 2018年 dashang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
